60a0f91046386327defa813f120a71e242f25be3
Pavel Flegr <pavelflegr@gmail.com>
2017-04-26 11:30:56 +0200

    .

--- WpfApp2/Model/Category.cs
+++ WpfApp2/Model/Category.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace WpfApp2.Model
 {
     class Category
     {
+        [PrimaryKey, AutoIncrement]
         int ID { get; set; }
         string Name { get; set; }
     }
